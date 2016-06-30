a0abc6f15c75bf0c4b0c374cdd5f90a5a70e642f
Sunghwan Cho <sunghwan.cho@atto-research.com>
2016-06-30 17:15:45 +0900

    check invaild username

--- athene/src/main/java/athene/testAthene.java
+++ athene/src/main/java/athene/testAthene.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class testAthene {
 	protected WebDriver driver;
+	JavascriptExecutor executor = (JavascriptExecutor)driver;
+	
 	private StringBuffer verificationErrors = new StringBuffer();
 	
 	@Before
@@ -32,8 +38,25 @@ public class testAthene {
 			WebElement username = driver.findElement(By.name("userid"));
 			WebElement passwd = driver.findElement(By.name("pw"));
 			WebElement login = driver.findElement(By.id("submit"));
-			Thread.sleep(5000);
+			Thread.sleep(1000);
 
+			
+			username.sendKeys("invaild");
+			Thread.sleep(1000);
+			passwd.sendKeys("invaild");
+			Thread.sleep(1000);
+			
+			login.submit();
+			Thread.sleep(3000);
+			
+			WebElement loginError = driver.findElement(By.className("ng-binding"));
+			String loginErrormsg = loginError.getAttribute("innerHTML");
+			//System.out.println(loginErrormsg);
+			
+			assertEquals(loginErrormsg, "invalid password or id");
+			
+			username.clear();
+			passwd.clear();
 
 			username.sendKeys("admin");
 			Thread.sleep(1000);
@@ -41,6 +64,15 @@ public class testAthene {
 			Thread.sleep(1000);
 			
 			login.submit();
+			Thread.sleep(3000);
+
+			
+            WebElement m_menu_service = driver.findElement(By.xpath("//*[contains(text(), 'Service')]"));
+            m_menu_service.click();
+            
+            WebElement s_menu_addnew = driver.findElement(By.xpath("//*[contains(text(), 'Add New')]"));
+            s_menu_addnew.click();
+            
 
 			} catch (Error e) {
 	            verificationErrors.append(e.toString());

3615de20603203d0210cc80d2f977f04b8b1c6f2
Sunghwan Cho <sunghwan.cho@atto-research.com>
2016-07-14 11:59:32 +0900

    delete comment line - close browser when testcase is finished

--- athene/src/main/java/athene/testServiceAddNew.java
+++ athene/src/main/java/athene/testServiceAddNew.java
@@ -247,7 +247,7 @@ public class testServiceAddNew {
 
 	@AfterTest
     public void tearDown() throws Exception {
-        //driver.quit();
+        driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
